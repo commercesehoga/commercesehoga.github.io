a739d7b49630b93a73c06ab8190b6cf62c5997ce
Thunderstudy <commercesehoga@gmail.com>
2026-04-11 22:13:38 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10862886
+    "zoneId": 10862907
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
